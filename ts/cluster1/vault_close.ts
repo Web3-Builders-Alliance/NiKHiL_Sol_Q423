43845da2f12a2c462f459e55e98846d6c7036846
NiKHiLkr23 <nikhilkumarofficial23@gmial.com>
2023-12-05 13:08:05 +0530

    refactor: vault_close updated

--- ts/cluster1/vault_close.ts
+++ ts/cluster1/vault_close.ts
@@ -12,14 +12,14 @@ import {
   Address,
   BN,
 } from "@coral-xyz/anchor";
-import { WbaVault, IDL } from "../programs/wba_vault";
+import { WbaVault, IDL } from "./programs/wba_vault";
 import wallet from "../wba-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 // Commitment
-const commitment: Commitment = "confirmed";
+const commitment: Commitment = "finalized";
 
 // Create a devnet connection
 const connection = new Connection("https://api.devnet.solana.com");
@@ -30,26 +30,36 @@ const provider = new AnchorProvider(connection, new Wallet(keypair), {
 });
 
 // Create our program
-const program = new Program<WbaVault>(IDL, "<address>" as Address, provider);
+const program = new Program<WbaVault>(
+  IDL,
+  "D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o" as Address,
+  provider
+);
 
 // Create a random keypair
-const vaultState = new PublicKey("<address>");
+const vaultState = new PublicKey("fwwwZggpfMqjPLCwPnvPtKMup3q16bs42nLw2RdZZVJ");
 
 // Create a random keypair
-// const closeVaultState = ???
+const closeVaultState = new PublicKey(
+  "7qd4HLjk7kXUXH9L2yRhMcBXq9aubBC2RRYJNkR4wN5a"
+);
 
 (async () => {
   try {
-    // const signature = await program.methods
-    // .closeAccount()
-    // .accounts({
-    //     ???
-    // })
-    // .signers([
-    //     keypair
-    // ]).rpc();
-    // console.log(`Close success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods
+      .closeAccount()
+      .accounts({
+        owner: keypair.publicKey,
+        closeVaultState,
+        vaultState,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(
+      `Close success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+    );
   } catch (e) {
-    console.error(`Oops, something went wrong: ${e}`);
+    console.error(`Oops, something went wrong:  ${JSON.stringify(e)}`);
   }
 })();
